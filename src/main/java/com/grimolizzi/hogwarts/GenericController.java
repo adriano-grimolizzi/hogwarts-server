01c52f31338b031754a33a8d4665e8711a009a45
Adriano Grimolizzi <adriano.grimolizzi@amadeus.com>
2019-09-13 15:54:22 +0200

    added generic post method in base controller

--- src/main/java/com/grimolizzi/hogwarts/GenericController.java
+++ src/main/java/com/grimolizzi/hogwarts/GenericController.java
@@ -1,27 +1,39 @@
 package com.grimolizzi.hogwarts;
 
-import java.util.Arrays;
+import com.google.gson.Gson;
+import com.grimolizzi.hogwarts.model.Wizard;
+import java.util.Collections;
 import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 public class GenericController<T> {
-	
-	protected GenericService<T> service;
-	
-	public GenericController(GenericService<T> service) {
+
+	private GenericService<T> service;
+	protected Class<T> type;
+
+	@SuppressWarnings("unchecked")
+	public GenericController(GenericService<T> service, Class<Wizard> type) {
 		this.service = service;
+		this.type = (Class<T>) type;
 	}
-	
+
 	@GetMapping()
 	public List<T> get(@RequestParam(required = false) String id) {
 		if (id != null) {
-			return Arrays.asList(this.service.get(id));
+			return Collections.singletonList(this.service.get(id));
 		} else {
 			return this.service.getAll();
 		}
+	}
+
+	@PostMapping()
+	public void create(@RequestBody String json) {
+
+		T t = new Gson().fromJson(json, this.type);
+
+		this.service.create(t);
 	}
 }
--- src/main/java/com/grimolizzi/hogwarts/wizards/WizardController.java
+++ src/main/java/com/grimolizzi/hogwarts/wizards/WizardController.java
@@ -1,24 +1,16 @@
 package com.grimolizzi.hogwarts.wizards;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.grimolizzi.hogwarts.GenericController;
 import com.grimolizzi.hogwarts.GenericService;
 import com.grimolizzi.hogwarts.model.Wizard;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/wizards")
 public class WizardController extends GenericController<Wizard> {
 
 	public WizardController(GenericService<Wizard> service) {
-		super(service);
-	}
-		
-	@PostMapping()
-	public void create(@RequestBody Wizard t) {
-		super.service.create(t);
+		super(service, Wizard.class);
 	}
 }
